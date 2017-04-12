37c2018c72bf725baab675098d9e23d167def713
Lucy Zhang <lucyzhang@Lucys-MacBook-Pro-2.local>
2017-04-11 23:31:35 -0400

    Initial Commit

---
+++ music-drawer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  music-drawer
+//
+//  Created by Lucy Zhang on 4/11/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ music-drawer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  music-drawer
+//
+//  Created by Lucy Zhang on 4/11/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
